cca752fd3cb6aeddb6f8afa1d1a75ab359a016bf
Sami Laine <laine.j.sami@gmail.com>
2015-07-26 14:53:02 +0300

    v0.1

--- js/sjl.track.js
+++ js/sjl.track.js
@@ -5,7 +5,8 @@
  *       - Cleanup
  */
 
-var api = 'http://rata.digitraffic.fi/api/v1/';
+var trafi = 'http://rata.digitraffic.fi/api/v1/';
+var meh = 'http://localhost/junat.eu/';
 var vr_georss = 'http://188.117.35.14/TrainRSS/TrainService.svc/AllTrains'
 
 // Clean this global shit up.
@@ -15,10 +16,20 @@ var operators;
 var stations;
 var trains;
 var types;
-var icons = { train: L.divIcon({ className: 'sjl-trains-train',
-                                 iconSize: [ 12, 25 ] }),
-              station: L.divIcon({ className: 'sjl-trains-station',
-                                   iconSize: [ 5, 5 ] }) };
+var trainIcons = { 
+  commuter: L.divIcon({ className: 'sjl-trains-train commuter',
+                        iconSize: [ 12, 25 ] }),
+  longdistance: L.divIcon({ className: 'sjl-trains-train long-distance',
+                            iconSize: [ 12, 25 ] }),
+};
+var stationIcons = {
+  generic: [ L.divIcon({ className: 'sjl-trains-station generic',
+                         iconSize: [ 5, 5 ] }), -100 ],
+  commuter: [ L.divIcon({ className: 'sjl-trains-station commuter',
+                        iconSize: [ 5, 5 ] }), 200 ],
+  person: [ L.divIcon({ className: 'sjl-trains-station person',
+                      iconSize: [ 5, 5 ] }), 100 ]
+};
 var timers = [];
 
 function getPrevStation(train) {
@@ -79,29 +90,41 @@ function getStationByName(id) {
     function(e, i) { return e.stationName == id; })[0];
 }
 
-function getTrafiMetas(ss) {
+function getMetas(ss, callBack) {
   var d = new Date();
-  var i = 1;
-  setTimeout(function() {
-               $.getJSON(api + '/compositions?date=' +
-                         d.getUTCFullYear() + "-" +
-                         d.getUTCMonth() + "-" +
-                         d.getUTCDay(),
-                         function(json) { compositions = json; }); }, 200);
-  setTimeout(function() {
-               $.getJSON(api + '/metadata/operator',
-               function(json) { operators = json; }); }, 200 * i++);
-  setTimeout(function() {
-               $.getJSON(api + '/metadata/station',
-               function(json) { stations = json;
-                                plotStations(ss, json); }); }, 200 * i++);
-  setTimeout(function() {
-               $.getJSON(api + '/metadata/train_type',
-               function(json) { types = json; }); }, 200 * i++);
+  $.getJSON(trafi + '/compositions?date=' +
+            d.getUTCFullYear() + "-" +
+            d.getUTCMonth() + "-" +
+            d.getUTCDay(),
+            function(json) {
+              compositions = json;
+            });
+  $.getJSON(trafi + '/metadata/operator',
+            function(json) {
+              operators = json;
+            });
+  $.getJSON(trafi + '/metadata/train_type',
+            function(json) { types = json; });
+  $.getJSON(trafi + '/metadata/station',
+            function(json) {
+              stations = json;
+            }).done(function(json) {
+              plotAllStations(ss, json)
+              callBack();
+            });
+}
+
+function getStationClasses(ls, ps) {
+  $.getJSON(meh + '/henkiloliikenteen-asemat.json',
+            function(json) {
+              plotPStations(ps, json, stationIcons['person']); });
+  $.getJSON(meh + '/lahiliikenteen-asemat.json',
+            function(json) {
+               plotCStations(ls, json, stationIcons['commuter']); });
 }
 
 function getTrains() {
-  $.getJSON(api + '/live-trains', function(json) { trains = json; });
+  $.getJSON(trafi + '/live-trains', function(json) { trains = json; });
 }
 
 function getVR(l, c) {
@@ -126,42 +149,35 @@ function updateVR(l, c, rss) {
 
   for(var i = coll.length - 1; i > 0; i--) {
     var dir = coll[i].getElementsByTagName('dir')[0].innerHTML;
-    var dst = coll[i].getElementsByTagName('to')[0].innerHTML;
-    var num = Number(coll[i].getElementsByTagName('guid')[0].
-                             innerHTML.replace(/[^\d]/g, ''));
     var lat = Number(coll[i].getElementsByTagName('point')[0].
                              innerHTML.split(' ')[0]);
     var lng = Number(coll[i].getElementsByTagName('point')[0].
                              innerHTML.split(' ')[1]);
-    var src = coll[i].getElementsByTagName('from')[0].innerHTML;
-    var typ = coll[i].getElementsByTagName('cat')[0].innerHTML;
-    var to = getStationByCode(dst);
-    var fr = getStationByCode(src);
+    var num = Number(coll[i].getElementsByTagName('guid')[0].
+                             innerHTML.replace(/[^\d]/g, ''));
+
+    var train = getTrainByNumber(num);
     var info;
-
     var label;
-    var trainInfo = getTrainByNumber(num);
+    var dst = getStationByUIC(train.timeTableRows[train.timeTableRows.length - 1].stationUICCode).stationName;
+    var src = getStationByUIC(train.timeTableRows[0].stationUICCode).stationName;
 
-    if (typ == 'H') {
-      label = getTrainByNumber(num).commuterLineID;
-      console.log('typeof(' + label + '): ' + typeof(label) );
+    if (train.trainCategory == 'Commuter') {
+      label = train.commuterLineID + ' (H' + train.trainNumber + ')';
       /* Commuter trains outside Helsinki region. */
-      if (typeof(label) == 'undefined')
-        label = typ + num;
-      info = label + ' ' +
-             fr.stationName + '-' + to.stationName + ' ' +
-             lat.toFixed(2) + '°N ' + lng.toFixed(2) + '°E'; 
+      icon = trainIcons['commuter'];
     } else {
-      label = typ + num;
-      info =  typ + num + ' ' +
-              fr.stationName + '-' + to.stationName + ' ' +
-              lat.toFixed(2) + '°N ' + lng.toFixed(2) + '°E'; 
+      label = train.trainType + num;
+      icon = trainIcons['longdistance'];
     }
+    info = label + ' ' +
+           src + '-' + dst + ' ' +
+           lat.toFixed(2) + '°N ' + lng.toFixed(2) + '°E'; 
 
     var mark = L.marker( [ lat, lng ],
                  { draggable: false,
                    clickable: true,
-                   icon: icons['train'],
+                   icon: icon,
                    opacity: 0.8,
                    riseOffset: 50,
                    riseOnHover: true,
@@ -172,7 +188,7 @@ function updateVR(l, c, rss) {
                                     offset: [ 12, -22 ] });
     mark.setIconAngle(dir);
 
-    if (typ == 'H')
+    if (train.trainCategory == 'Commuter')
       cs.push(mark);
     else
       ls.push(mark);
@@ -183,7 +199,7 @@ function updateVR(l, c, rss) {
   l.addLayer(L.layerGroup(ls));
 }
 
-function plotStations(ss, json) {
+function plotAllStations(group, json) {
   var ms = [];
 
   for(var i = 0; i < stations.length; i++) {
@@ -192,16 +208,78 @@ function plotStations(ss, json) {
                stations[i].latitude.toFixed(2) + '°N' + ', ' +
                stations[i].longitude.toFixed(2) + '°E';
     ms.push( L.marker( [ stations[i].latitude, stations[i].longitude ],
-      { draggable: false,
+      {
+        draggable: false,
         clickable: true,
         html: stations[i].stationName,
-        icon: icons['station'],
+        icon: stationIcons['generic'][0],
         opacity: 0.8,
         raiseOnHover: false,
-        title: info }));
+        title: info,
+        zIndexOffset: stationIcons['generic'][1],
+      }));
   }
-  ss.clearLayers();
-  ss.addLayer(L.layerGroup(ms));
+  group.clearLayers();
+  group.addLayer(L.layerGroup(ms));
+}
+
+function plotPStations(group, arr, icon) {
+  var ms = [];
+
+  for(var i = 0; i < arr.length - 1; i++) {
+    var s = getStationByCode(arr[i]);
+    var info = s.stationName +
+               ' (' + s.stationShortCode +') ' +
+               s.latitude.toFixed(2) + '°N' + ', ' +
+               s.longitude.toFixed(2) + '°E';
+    ms.push( L.marker( [ s.latitude, s.longitude ],
+      {
+        draggable: false,
+        clickable: true,
+        html: s.stationName,
+        icon: icon[0],
+        opacity: 0.8,
+        raiseOnHover: false,
+        title: info,
+        zIndexOffset: icon[1]
+      }));
+  }
+  group.clearLayers();
+  group.addLayer(L.layerGroup(ms));
+}
+
+function plotCStations(group, obj, icon) {
+  var ms = [];
+  var ss = [];
+
+  lahi = obj;
+  jQuery.each(obj,
+              function(n, o) {
+                ss = ss.concat(o.filter(function(e) {
+                                           return ss.indexOf(e) == -1;
+                                         }));
+              });
+
+  for(var i = 0; i < ss.length - 1; i++) {
+    var s = getStationByCode(ss[i]);
+    var info = s.stationName +
+               ' (' + s.stationShortCode +') ' +
+               s.latitude.toFixed(2) + '°N' + ', ' +
+               s.longitude.toFixed(2) + '°E';
+    ms.push( L.marker( [ s.latitude, s.longitude ],
+      {
+        draggable: false,
+        clickable: true,
+        html: s.stationName,
+        icon: icon[0],
+        opacity: 0.8,
+        raiseOnHover: false,
+        title: info,
+        zIndexOffset: icon[1]
+      }));
+  }
+  group.clearLayers();
+  group.addLayer(L.layerGroup(ms));
 }
 
 $().ready(function() {
@@ -210,32 +288,39 @@ $().ready(function() {
     { attribution:
         '<a href="https://github.com/samilaine/junat.eu">Junat Kartalla</a>' +
         ' &copy; <a href="http://github.com/samilaine">' +
-        'Sami Laine</a> ' +
+        'Sami Laine</a> | ' +
         'Map data © <a href="http://openstreetmap.org">' +
         'OpenStreetMap</a> contributors' } );   
 
+  var ls = new L.layerGroup();
+  var ps = new L.layerGroup();
   var ss = new L.layerGroup();
   var lts = new L.layerGroup();
   var cts = new L.layerGroup();
 
-  map = L.map('map', { center: [ 61.360, 24.994 ],
-                       layers: [ osmBW, ss, lts, cts ],
+  map = L.map('sjl-trains-map', { center: [ 60.860, 24.994 ],
+                       layers: [ osmBW, ps, lts, cts ],
                         zoom: 7 });
 
-  L.control.layers(null, { 
-                           'Kaukojunat': lts,
-                           'Liikennepaikat': ss,
-                           'Lähijunat': cts,
+  L.control.layers({
+                   },
+                   { 
+                     'Henkilöliikenteen asemat': ps,
+                     'Kaikki liikennepaikat': ss,
+                     'Lähiliikenteen asemat': ls,
+                     'Kaukojunat': lts,
+                     'Lähijunat': cts,
                    }).addTo(map);
 
   if (L.Browser.touch)
     L.control.touchHover().addTo(map);
 
   getTrains();
-  getTrafiMetas(ss);
+  getMetas(ss, function() { getStationClasses(ls, ps); });
 
   timers.push(setInterval(function() {
-                            getTrafiMetas(ss); }, 1000 * 60 * 15));
+                            getMetas(ls, ps, ss, function() {});
+                          }, 1000 * 60 * 15));
   timers.push(setInterval(function() { getTrains(); }, 1000 * 120));
   timers.push(setInterval(function() { getVR(lts, cts); }, 1000 * 5));
 });
